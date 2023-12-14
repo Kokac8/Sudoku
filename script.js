842c9c9d65f892cd468a33f02a820062c1cc01ee
Kokac8 <86925980+Kokac8@users.noreply.github.com>
2023-12-14 15:08:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,36 @@ function generateSudoku() {
     // Sudoku generation logic will go here
     alert("Generate a new Sudoku puzzle!");
 }
+
+// A pre-defined puzzle for illustration purposes
+// 0 represents an empty cell
+const samplePuzzle = [
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9]
+];
+
+// Function to populate the grid with a Sudoku puzzle
+function populateSudokuGrid(puzzle) {
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      const cell = document.getElementById(`cell-${row}-${col}`);
+      if (puzzle[row][col] !== 0) {
+        cell.textContent = puzzle[row][col];
+      } else {
+        cell.textContent = '';
+        cell.contentEditable = true; // Make the cell editable
+      }
+    }
+  }
+}
+
+// Call the function to populate the grid with the sample puzzle
+populateSudokuGrid(samplePuzzle);
+
