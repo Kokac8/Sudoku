980d96ee2816445b497b5cff97130f6e74ed92b9
Kokac8 <86925980+Kokac8@users.noreply.github.com>
2023-12-14 15:40:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // A pre-defined puzzle for illustration purposes
 // 0 represents an empty cell
 const samplePuzzle = [
-  // ... (sample puzzle remains unchanged)
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ];
 
 // Stack to keep track of user actions for undo and redo
@@ -10,17 +18,40 @@ let redoStack = [];
 
 // Function to populate the grid with a Sudoku puzzle
 function populateSudokuGrid(puzzle) {
-  // ... (populate function remains unchanged)
+ for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      const cell = document.getElementById(`cell-${row}-${col}`);
+      if (puzzle[row][col] !== 0) {
+        cell.textContent = puzzle[row][col];
+        cell.contentEditable = false;
+      } else {
+        cell.textContent = '';
+        cell.contentEditable = true; // Make the cell editable
+      }
+    }
+  }
 }
 
 // Function to check if a number can be placed in a cell
 function isNumberValid(number, row, col, puzzle) {
-  // ... (validation function remains unchanged)
+   // Check the row and column
+  for (let i = 0; i < 9; i++) {
+    if (puzzle[row][i] === number || puzzle[i][col] === number) {
+      return false;
+    }
 }
 
 // Function to update the puzzle array when a user inputs a number
 function updatePuzzleWithInput(row, col, value) {
-  // ... (update function remains unchanged)
+ // Check if the entered value is between 1 and 9
+  if (value >= 1 && value <= 9) {
+    // Check if the number is valid in the current puzzle state
+    if (isNumberValid(value, row, col, samplePuzzle)) {
+      samplePuzzle[row][col] = value; // Update the puzzle array
+      return true;
+    }
+  }
+  return false;
 }
 
 // Function to push an action to the stack
@@ -31,17 +62,51 @@ function pushAction(row, col, prevValue, newValue) {
 
 // Undo function
 function undo() {
-  // ... (undo function remains unchanged)
+    const action = actionStack.pop();
+    if (action) {
+        redoStack.push({
+            row: action.row,
+            col: action.col,
+            prevValue: samplePuzzle[action.row][action.col],
+            newValue: action.prevValue
+        });
+        // Revert the cell to its previous value
+        samplePuzzle[action.row][action.col] = action.prevValue;
+        const cell = document.getElementById(`cell-${action.row}-${action.col}`);
+        cell.textContent = action.prevValue === 0 ? '' : action.prevValue;
+    }
 }
 
 // Redo function
 function redo() {
-  // ... (redo function remains unchanged)
+  const action = redoStack.pop();
+    if (action) {
+        pushAction(action.row, action.col, action.prevValue, action.newValue);
+        // Reapply the value from the redo action
+        samplePuzzle[action.row][action.col] = action.newValue;
+        const cell = document.getElementById(`cell-${action.row}-${action.col}`);
+        cell.textContent = action.newValue === 0 ? '' : action.newValue;
+    }
 }
 
 // Check Puzzle function
 function checkPuzzle() {
-  // ... (check puzzle function remains unchanged)
+  for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            const value = samplePuzzle[row][col];
+            // Temporarily set the cell's value to 0 to exclude it from validation checks
+            samplePuzzle[row][col] = 0;
+            if (value !== 0 && !isNumberValid(value, row, col, samplePuzzle)) {
+                alert('The puzzle is incorrect!');
+                // Don't forget to reset the value after checking
+                samplePuzzle[row][col] = value;
+                return;
+            }
+            // Reset the value after checking
+            samplePuzzle[row][col] = value;
+        }
+    }
+    alert('Congratulations! The puzzle is correct!');
 }
 
 // Extend the event listener to push actions to the stack
